aa02813719bb91058ecf557a106e520f3074171b
techno-cardi <tremblay.kevin@cscapitale.qc.ca>
2026-08-30 21:39:16 -0400

    Utiliser la version serveur stable finale

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.ATELIER_CONFIG = {
-  backendUrl: 'https://script.google.com/macros/s/AKfycbxNvvZ4RT8GXPiyzoKt1rgPsfx1N0ZHAG2GlZbhReNYwgC5lODVmkSRiT9zw5KQJsBb/exec',
+  backendUrl: 'https://script.google.com/macros/s/AKfycbzfWoKG4vrwahyRCkjmctNUhR7N598-XpmGyvMWuduRzCsiOyvcBvuGlyLgqe18k6fj/exec',
 };
